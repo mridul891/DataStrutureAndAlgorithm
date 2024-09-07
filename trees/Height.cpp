464888754bf329d8a89ee28b0a97738700a964f4
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-09-07 10:28:52 +0530

    Height of tree

---
+++ trees/Height.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+int HeightOfTree(Node *root)
+{
+    if (root == NULL)
+        return 0;
+
+    int lh = HeightOfTree(root->left);
+    int rh = HeightOfTree(root->right);
+
+    return 1+ max(lh+rh);
+}
