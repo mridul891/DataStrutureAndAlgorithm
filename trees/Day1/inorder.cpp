6aa65704731e11dba427ea2cb710647a7e8258e8
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-09-07 09:42:40 +0530

    Inorder TRaversal Done

--- trees/Day1/inorder.cpp
+++ trees/Day1/inorder.cpp
@@ -2,16 +2,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<int> ans;
-void in(TreeNode<int> *root)
+struct Node
+{
+    int val;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        val = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+void inorderfunction(Node *root)
 {
     if (root == NULL)
         return;
-    in(root->left);
-    ans.push_back(root->data);
-    in(root->right);
-}
-vector<int> inOrder(TreeNode<int> *root)
-{
-    in(root);
-    return v;
+    inorderfunction(root->left);
+    cout << root->val << endl;
+    inorderfunction(root->right);
 }
