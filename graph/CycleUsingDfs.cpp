a46257b9491a879163227605b2c397d8a87501e8
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-10-06 21:50:31 +0530

    Cycle using dfs

---
+++ graph/CycleUsingDfs.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+bool CycleDetected(int node, int parent, vector<int> &visited, vector<bool> adj[])
+{
+    visited[node] = 1;
+    for (int j = 0; j < adj[node].size(); j++)
+    {
+        if (parent == adj[node][j])
+            continue;
+        if (visited[adj[node][j]] == 1)
+            return 1;
+        if (CycleDetected(adj[node][j], node, visited, adj))
+            return 1;
+    }
+
+    return 0;
+}
+
+bool isCycle(int v, vector<int> adj[])
+{
+    vector<bool> visited(v, 0);
+    return CycleDetected(0, -1, visited, adj);
+}
